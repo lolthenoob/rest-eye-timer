d7f27ef4fb2c6650fe074e6041105df6d8517370
lolthenoob <40536747+lolthenoob@users.noreply.github.com>
2018-07-04 12:29:53 +1200

    Add files via upload

---
+++ Timer_01_07_2018 -form 2/Timer/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timerProject
+{
+    public partial class Form2 : Form
+    {
+        public int currentTime = 0;
+        
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void SECbtnDone_Click(object sender, EventArgs e)
+        {
+            SECtimer.Stop();
+            this.Hide();
+           
+        }
+
+        private void SECtimer_Tick(object sender, EventArgs e)
+        {
+            currentTime++;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+        }
+    }
+}
